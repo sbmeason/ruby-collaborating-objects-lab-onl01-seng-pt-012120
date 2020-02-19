764a675470fc53198c7729d57d2d85f439ec5a5a
Samantha Meason <sbmeason@gmail.com>
2020-02-19 23:24:02 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,21 @@
 class MP3Importer
-  attr_reader :path
+  attr_accessor :path
 
-    def initialize(path)
-      @path = path
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
     end
-    
-    def files
-      @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
     end
-    
-    def import
-      self.files.each{|file| Song.new_by_filename(file)}
-    end
+  end
 end

a5f9f9b713e63478ee14e7943b40d3bc713c9ff7
Samantha Meason <sbmeason@gmail.com>
2020-02-20 00:10:26 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []  
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.save
+  end
+
+  def add_song(song)
+    self.songs << song    
+  end
+
+  def self.find_or_create_by_name(name) 
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)  
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all      
+    @@all
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
